cc73380a8e438a48a0226fb0e10cc2e0c6997d0e
luoxiangyu <luoxy@sobot.com>
2024-07-27 19:02:30 +0800

    chore: remove sortable types

--- src/monaco-editor/index.ts
+++ src/monaco-editor/index.ts
@@ -5,20 +5,6 @@ export * from './hooks/useMonaco';
 
 export * from './Editor';
 
-export {
-  DOMRect,
-  Direction,
-  GroupOptions,
-  MoveEvent,
-  Options,
-  PullResult,
-  PutResult,
-  default as Sortable,
-  SortableEvent,
-  SortableOptions,
-  Utils,
-} from 'sortablejs';
-
 // Monaco
 import type * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 export type Monaco = typeof monaco;
